7ac94bb34e178f0d6f8fb8375706bfdfa5b2028c
whfuyn <whfuyn@outlook.com>
2020-12-09 16:30:14 +0800

    don't need to build server

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     tonic_build::configure()
         .build_client(false)
-        .build_server(true)
+        .build_server(false)
         .format(true)
         .compile(
             &[
