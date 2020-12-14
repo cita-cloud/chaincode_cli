f922f62a582e0afbad60bb50a340c1288dc33a3b
whfuyn <whfuyn@outlook.com>
2020-12-14 17:34:33 +0800

    use tutorial's cmds

--- examples/asset-transfer-basic.rs
+++ examples/asset-transfer-basic.rs
@@ -6,10 +6,8 @@ async fn main() {
     let controller_addr = "localhost:50004";
     let (mut org, _) =
         Invoker::default_orgs("asset-transfer-basic", kms_addr, controller_addr).await;
-    org.call("InitLedger", &["asset8", "blue", "16", "Kelly", "750"], &[])
-        .await;
-    org.call("ReadAsset", &["asset8"], &[]).await;
+    org.call("InitLedger", &[], &[]).await;
     org.call("GetAllAssets", &[], &[]).await;
-    org.call("TransferAsset", &["asset1", "Alice"], &[]).await;
-    org.call("ReadAsset", &["asset1"], &[]).await;
+    org.call("TransferAsset", &["asset6", "Christopher"], &[]).await;
+    org.call("ReadAsset", &["asset6"], &[]).await;
 }
